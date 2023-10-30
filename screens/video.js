54eef22db00f20c0219ee74f3b53626ffda23e47
Andre <syahandrean59@gmail.com>
2023-10-30 20:06:02 +0700

    baruu

---
+++ screens/video.js
@@ -0,0 +1,17 @@
+import { Heading, Center } from "native-base";
+import { Header } from "../components/";
+
+const Video = () => {
+  return (
+    <>
+      <Header title={"Video"} />
+      <Center flex={1}>
+        <Heading>Video</Heading>
+      </Center>
+    </>
+  );
+};
+
+export default Video;
+
+
